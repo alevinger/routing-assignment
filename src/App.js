0cc708ee8a092071a920951ab141907565108782
alevinger <ayala.levinger@gmail.com>
2019-02-06 14:50:26 +0100

    upload assignment files

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, NavLink, Switch, Redirect} from 'react-router-dom';
+
+import Courses from './containers/Courses/Courses';
+import Users from './containers/Users/Users';
+import Course from './containers/Course/Course';
+
+class App extends Component {
+  render () {
+    return (
+
+      <BrowserRouter>
+        <div className="App">
+          <header>
+              <nav>
+                <ul>
+                  <li><NavLink to="/users">Users</NavLink></li>
+                  <li><NavLink to="/courses">Courses</NavLink></li>
+                </ul>
+              </nav>
+          </header>
+
+          <Redirect from='/all-courses' to='/courses'/>
+
+          <Switch>
+            <Route path="/users" component={Users} />
+            <Route path="/courses/course/:id" exact component={Course} />
+            <Route path="/courses" component={Courses} />
+            <Route render={ ()=> <h1>404 - Page Not found</h1>}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
+
+
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/Courses/Courses.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+import './Courses.css';
+
+class Courses extends Component {
+    state = {
+        courses: [
+            { id: 1, title: 'Angular - The Complete Guide' },
+            { id: 2, title: 'Vue - The Complete Guide' },
+            { id: 3, title: 'PWA - The Complete Guide' }
+        ]
+    }
+
+    openCoursePage = (id, title) => {
+        this.props.history.push('/courses/course/' + id + "?title=" + title);
+    }
+
+
+    render () {
+        return (
+            <div>
+                <h1>Amazing Udemy Courses</h1>
+                <section className="Courses">
+                    {
+                        this.state.courses.map( course => {
+                            return (
+                              <article
+                                onClick={() => this.openCoursePage(course.id, course.title)}
+                                className="Course"
+                                key={course.id}>{course.title}
+
+                              </article>
+                            );
+                        } )
+                    }
+                </section>
+
+            </div>
+        );
+    }
+}
+
+export default Courses;
